e370a6d4436bd8555b910b784cb8299bbdac6496
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-07-05 21:19:38 +0300

    step

---
+++ lesson2/todo-list/rewiev_build/precache-manifest.e8e357e127ca4c79c8e88087c89f457b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80d88cd6a67793b9058ed73d94a12fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff9cafc2f4b75535f5c",
+    "url": "/static/css/main.0b32da9a.chunk.css"
+  },
+  {
+    "revision": "f4cd8603cbc98fa8da98",
+    "url": "/static/js/2.730fe4ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.730fe4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff9cafc2f4b75535f5c",
+    "url": "/static/js/main.8cfcad2f.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  }
+]);
