aa4d4ad3d7db1d9d7aa88886ee1806e47ee48aec
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-07-07 23:32:41 +0300

    stepy

--- lesson4/task1/src/App.jsx
+++ lesson4/task1/src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import Counter from './Counter';
 
 const App = ()=>{
-    return (
-    <Counter start ={17} interval ={1000}/>
+    return (<>
+    <Counter start ={17} interval ={2000}/>
+    <Counter start ={30} interval ={1000}/>
+    <Counter start ={-7} interval ={100}/>
+    </>
     )
 }
 export default App
