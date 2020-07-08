38be7ca4a67df816b0ea3f6244753d03b309a3e9
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-07-08 22:40:27 +0300

    if

---
+++ lesson6/task3/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Page from './Page.jsx';
+import './index.scss';
+
+
+
+
+const rootElement=document.querySelector('#root');
+
+ReactDOM.render(<Page />, rootElement)
