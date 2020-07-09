371522717285d810ce37b2313ad1ed2d8ab70912
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-07-09 17:14:40 +0300

    stepByStep

---
+++ lesson6/hw2/src/Auth.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import Login from './Login'
+import Spinner from './Spinner'
+import Logout from './Logout'
+
+class Auth extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoggedIn: false,
+            isProcessing: false,
+        }
+    }
+
+
+    login = () => {
+        this.setState({
+            isProcessing: true
+        })
+
+
+        setTimeout(()=>{
+            this.setState({
+                isProcessing:false,
+                isLoggedIn: true
+            })
+        },2000)
+      
+
+
+    }
+
+    logout = () => {
+        this.setState({
+            isLoggedIn: false
+        })
+
+    }
+
+
+
+    render() {
+
+        const { isLoggedIn, isProcessing } = this.state;
+
+        if (isProcessing) {
+            return (
+                <Spinner size={77}/>
+            )
+        }
+
+        if (isLoggedIn) {
+            return (
+                <Logout onLogout={this.logout} />)
+        }
+        return (<Login onLogin={this.login} />)
+
+
+
+
+    }
+
+
+}
+
+export default Auth
---
+++ lesson6/hw2/src/Logout.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logout = (props) => {
+    const onLogut = props.onLogout
+
+    return (
+        <button className="logout btn" onClick={onLogut}>Logout</button>)
+}
+export default Logout
