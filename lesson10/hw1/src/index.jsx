93bec37374d32db8f7de0d5477549a494c39dc9f
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-07-19 09:35:31 +0300

    stepyStep

---
+++ lesson10/hw1/src/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+import Calculator from './Calculator'
+
+
+
+const rootElement = document.querySelector('#root');
+
+
+
+
+ReactDOM.render(<Calculator />, rootElement);
+
+
+
+
+
+
