8f335a7ce947e42a3e431d3a73cf84ea5d044fad
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-08-14 19:48:57 +0300

    qwe

--- redux/lesson1/hw1/src/App.jsx
+++ redux/lesson1/hw1/src/App.jsx
@@ -5,21 +5,18 @@ import { userData, NameContext } from './data-context';
 
 class App extends Component {
     state = {
-        userData: {
-            name: 'Nikola Tesla',
-            avatar_url: 'https://avatars3.githubusercontent.com/u10001'
-        },
+        userData: userData,
     }
 
 
     render() {
         console.log(this.state.userData)
         return (
-            <div class="page">
+            <div className="page">
                 <NameContext.Provider value={this.state.userData}>
                     <Header />
                 </NameContext.Provider>
-            </div>
+            </div>    
 
         )
     }
--- redux/lesson1/hw1/src/UserMenu.jsx
+++ redux/lesson1/hw1/src/UserMenu.jsx
@@ -6,7 +6,7 @@ class UserMenu extends Component {
     render() {
         return (
             <div className="menu">
-                <span className="menu__greeting">{`Hello,${this.context.name }`}</span>
+                <span className="menu__greeting">{`Hello, ${this.context.name }`}</span>
                 <img alt="User Avatar" src={this.context.avatar_url}  className="menu__avatar" />
             </div>
         )
