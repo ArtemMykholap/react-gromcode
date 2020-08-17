aab6b0fa5a4725f4f64b0b86180991b7b4dd40ee
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-08-17 19:19:15 +0300

    step

--- redux/lesson2/hw1/src/index.js
+++ redux/lesson2/hw1/src/index.js
@@ -5,7 +5,5 @@ store.dispatch(addUser({ name: 'Kim', id: 63 }))
 store.dispatch(deleteUser(63))
 store.dispatch(addUser({ name: 'George', id: 89 }))
 store.dispatch(addUser({ name: 'Tom', id: 777 }))
-
-
 const usersList = store.getState()
 console.log(usersList)
