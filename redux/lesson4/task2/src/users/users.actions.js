e18516177a4a186c93416f2455db87d31dd6d3ee
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-08-24 11:20:12 +0300

    stepByStep

---
+++ redux/lesson4/task2/src/users/users.actions.js
@@ -0,0 +1,33 @@
+export const ADD_USER = 'USER/ADD_USER';
+export const DELETE_USER = 'USER/DELETE_USER';
+export const UPDATE_USER = 'USER/UPDATE_USER';
+
+
+export const addUser = (userData) => {
+    return {
+        type: ADD_USER,
+        payload: {
+            userData
+        }
+    };
+}
+
+export const updateUser = (userId, userData) => {
+    return {
+        type: UPDATE_USER,
+        payload: {
+            userId,
+            userData,
+        }
+    };
+}
+
+export const deleteUser = (userId, userData) => {
+    return {
+        type: DELETE_USER,
+        payload: {
+            userId,
+            userData,
+        }
+    };
+}
