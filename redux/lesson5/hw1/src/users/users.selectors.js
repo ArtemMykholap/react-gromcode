dfe5f293c9b56dc87432c47b2c8658778e2ce009
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-09-02 10:37:24 +0300

    stepByStep

--- redux/lesson5/hw1/src/users/users.selectors.js
+++ redux/lesson5/hw1/src/users/users.selectors.js
@@ -1,8 +1,9 @@
+// import { createSelector } from 'reselect'
+
 export const usersListSelector = state => {
     return state.usersList
 
 };
-
 
 export const filterTextSelector = state => {
     return state.filterText
